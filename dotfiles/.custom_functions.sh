42661087303beea1f1dba10bc7598babaaa8e6f7
Kyle Bebak <kylebebak@gmail.com>
2016-05-18 11:57:53 -0500

    changed name of function gbpf so that it's meaning was more clear

--- dotfiles/.custom_functions.sh
+++ dotfiles/.custom_functions.sh
@@ -141,7 +141,7 @@ function ghpf(){
 alias gdp="git diff --name-only | pick | xargs git diff"
 
 # pick other, compare files with current, then pick one of these files
-function gdbp(){
+function gbpf(){
   cd $(git rev-parse --show-toplevel) # cd into root of repo, otherwise git diff for file won't work
   other=$(_pick_branch)
   git diff $other --name-only | pick | xargs git diff $other --
