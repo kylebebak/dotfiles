3fdb80fc426ba2c67bf8ad6c4e322a071bae6e78
Kyle Bebak <kylebebak@gmail.com>
2019-05-22 10:40:54 -0500

    Adds .prettierrc.js

--- dotfiles/.eslintrc.js
+++ dotfiles/.eslintrc.js
@@ -1,46 +1,28 @@
 module.exports = {
-    "globals": {
-        "$": true,
-        "google": true,
+  globals: {
+    $: true,
+    google: true,
+  },
+  rules: {
+    indent: [1, 2],
+    quotes: [1, 'single'],
+    'linebreak-style': [2, 'unix'],
+    semi: [0, 'always'],
+    'comma-dangle': [0],
+    'no-unused-vars': [1],
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    "rules": {
-        "indent": [
-            1,
-            2
-        ],
-        "quotes": [
-            1,
-            "single"
-        ],
-        "linebreak-style": [
-            2,
-            "unix"
-        ],
-        "semi": [
-            0,
-            "always"
-        ],
-        "comma-dangle": [
-            0,
-        ],
-        "no-unused-vars": [
-            1,
-        ]
-    },
-    "env": {
-        "es6": true,
-        "browser": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 6,
-        "sourceType": "module",
-        "ecmaFeatures": {
-            "jsx": true
-        },
-    },
-    "plugins": [
-        "react"
-    ]
-};
+  },
+  plugins: ['react'],
+}
