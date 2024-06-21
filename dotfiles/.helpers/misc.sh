22974b019a804af4e16a9c120f191124dfc42202
Kyle Bebak <kyle@elementaryml.com>
2024-06-21 11:34:16 +0200

    - Remove some unused config

--- dotfiles/.helpers/misc.sh
+++ dotfiles/.helpers/misc.sh
@@ -1,4 +1,4 @@
-# get N most recently modified files within current directory and all subdirectories
+# Get N most recently modified files within current directory and all subdirectories
 function mrm() {
   bold=`tput bold`
   normal=`tput sgr0`
@@ -10,22 +10,17 @@ function mrm() {
   done
 }
 
-function copyhist() {
-  history | tail -r | pbcopy
-}
-
+# Get top $2 entries under directory $1 by disk usage
 function dum() {
   du -m $1 | sort -n | tail -n ${2:-10}
 }
 
-# tar -zxvf Code.tar.gz
-alias archivecode='cd $HOME && tar --exclude "node_modules" -zcvf Code.tar.gz Code && mv Code.tar.gz GoogleDrive'
-
+# Get $1 most frequently modified files according to git log
 function churn() {
   git log --format=format: --name-only --since=6.month \
  | egrep -v '^$' \
  | sort \
  | uniq -c \
  | sort -nr \
- | head -30
+ | head -${1:-30}
 }
--- dotfiles/.helpers/nvm.sh
+++ dotfiles/.helpers/nvm.sh
@@ -4,7 +4,7 @@ function load_nvm() {
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 }
 
-# run nvm without having to load it on shell startup
+# Run nvm without having to load it on shell startup
 function nvm() {
   echo 'loading nvm'
   load_nvm
